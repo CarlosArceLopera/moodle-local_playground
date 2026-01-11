a73b58d9a3970e7d45499ab73959dfe91ebf7d5c
thibaud <thibaud@yorku.ca>
2026-01-11 11:02:08 -0500

    Removed unused strings. Updated french strings

--- lang/en/local_playground.php
+++ lang/en/local_playground.php
@@ -15,16 +15,11 @@
 $string["category"] = "Category";
 $string["category_help"] = "Select the category you would like playground courses to be created in.";
 $string["completed"] = "Completed";
-$string["create_playground"] = "Create Playground";
 $string["create_playground_course"] = "Create playground course";
 $string["enrol_confirmation_subject"] = "Your Playground course has been created";
 $string["enrol_confirmation_body"] = 'Hello,<br /><br />We\'ve created your course and added you as an instructor. You can access it at {$a->link}<br /><br />Please note, this course should only be used to prepare your content for a future course you have not yet been assigned to, do not add students to this course or run the course from here. Once you have been assigned to your official course offering you can move any content created in this course to that course using the {$a->link2}.<br /><br />Learning Technology Services';
-$string["new_request"] = "New request";
-$string["not_allowed"] = "You do not have permission to view this page.";
 $string["pluginname"] = "Playground creation";
 $string["privacy:metadata"] = "This plugin does not store any user data. It simply creates courses and enrols the logged in user.";
-$string["playground"] = "Sandbox";
 $string["playground_completed_text"] = "Your new playground course has been created. Click the button below to access your course.<br>";
-$string["playground_course_creation"] = "Your playground course is being created.";
 $string["playground_course_name"] = "Playground course";
 
--- lang/fr/local_playground.php
+++ lang/fr/local_playground.php
@@ -12,17 +12,14 @@
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
  * *************************************************************************
  * *************************************************************************/
-$string["completed"] = "Demande terminée.";
-$string["create_playground"] = "Create Playground";
-$string["create_playground_course"] = "Create playground course";
-$string["enrol_confirmation_body"] = "Ceci est pour confirmer que vous avez été inscrit en cours ";
-$string["enrol_confirmation_subject"] = "Inscription à un nouveau cours";
-$string["new_request"] = "Nouvelle requête";
-$string["not_allowed"] = "Vous n'avez pas assez de privilèges. Vous serez redirigé vers la page d'accueil.";
-$string["pluginname"] = "Outil de création de cours";
-$string["privacy:metadata"] = "Ce plugin ne stocke aucune donnée utilisateur. Il crée simplement des cours et inscrit l'utilisateur connecté.";
-$string["playground"] = "Bac de sable";
-$string["playground_completed_text"] = "Votre nouveau bac à sable a été créé.";
-$string["playground_course_creation"] = "Votre cours bac à sable est en cours de création.";
+$string["category"] = "Catégorie";
+$string["category_help"] = "Sélectionnez la catégorie dans laquelle vous souhaitez que les cours de bac à sable soient créés.";
+$string["completed"] = "Terminé";
+$string["create_playground_course"] = "Créer un cours de bac à sable";
+$string["enrol_confirmation_body"] = "Bonjour,<br /><br />Nous avons créé votre cours et vous avez été ajouté comme instructeur. Vous pouvez y accéder à {$a->link}<br /><br />Veuillez noter que ce cours ne doit être utilisé que pour préparer votre contenu pour un cours futur que vous n\'avez pas encore été assigné à, n\'ajoutez pas d\'étudiants à ce cours et ne l\'exécutez pas à partir d\'ici. Une fois que vous avez été assigné à votre offre de cours officiel, vous pouvez déplacer tout contenu créé dans ce cours vers ce cours en utilisant {$a->link2}.<br /><br />Services de technologie d\'apprentissage";
+$string["enrol_confirmation_subject"] = "Votre cours de bac à sable a été créé";
+$string["pluginname"] = "Création de bac à sable";
+$string["privacy:metadata"] = "Ce plugin ne stocke aucune donnée utilisateur. Il crée simplement des cours et inscrit l\'utilisateur connecté.";
+$string["playground_completed_text"] = "Votre nouveau cours de bac à sable a été créé. Cliquez sur le bouton ci-dessous pour accéder à votre cours.<br>";
 $string["playground_course_name"] = "Cours de bac à sable";
 
